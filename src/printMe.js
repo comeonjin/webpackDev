86941bf2dff146cf54324f954d8445f2043135ec
zhangjin <zhangjin@gaosiedu.com>
2018-03-24 02:29:06 +0800

    add console.log

--- src/printMe.js
+++ src/printMe.js
@@ -1,3 +1,4 @@
 export default function printMe(){
     console.log("I get called from printMe.js! now changes will be watched by webpack-dev-server")
+    console.log("do some changes for ssh to github")
 }
